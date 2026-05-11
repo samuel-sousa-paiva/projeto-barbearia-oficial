e6b481a34c95293b3dfa912fa7aabaa5e4bc75a5
João <samuelsousapaiva16@gmail.com>
2026-05-11 10:36:04 -0300

    fiz algumas alteraçoes com o nome

--- entrada.py
+++
@@ -1,9 +0,0 @@
-print('Digite sua idade:')
-idade = int(input())
-
-if idade < 18:
-    print('Você é menor de idade!')
-elif idade == 18:
-    print('Você tem 18 anos!')
-else:
-    print('Você é maior de idade!')

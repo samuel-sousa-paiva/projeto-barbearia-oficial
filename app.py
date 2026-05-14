7d65ce7e1994fb3a40ed6000c98790bd17645433
Myriam M. <samuelsousapaiva16@gmail.com>
2026-05-14 09:26:49 -0300

    Fiz algumas alteraçoes

--- app.py
+++ app.py
@@ -129,3 +129,23 @@ def agendar():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
+    
+    from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
+
+@app.route("/cadastro_ok")
+def cadastro_ok():
+    return render_template("cadastro_ok.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
